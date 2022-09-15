1a11cd45c1ee95dc69c64ffe958030b575c25286
AnsulaDev <ansulaoari88@gmail.com>
2022-09-15 19:33:05 +0530

    added cookies sessions

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const express = require('express');
 const helmet = require('helmet');
 const passport = require('passport');
 const { Strategy } = require('passport-google-oauth20');
+const cookieSession = require('cookie-session');
+const { verify }  = require('crypto');
 require('dotenv').config();
 
 
@@ -13,6 +15,8 @@ const PORT = 3000;
 const config = {
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 
 };
 const AUTH_OPTIONS = {
@@ -28,9 +32,34 @@ function verifyCallback(accessToken, refreshToken, profile, done) {
 }
 
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallback ));
+
+//save the session to the cookie
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+
+});
+
+// read the session from the cookie
+passport.deserializeUser((id, done) => {
+/*  User.findById(id).then(user => {
+        done(null, user);
+    }) */
+    done(null,id );
+});
 const app = express();
 app.use(helmet());
-app.use(passport.initialize());
+
+app.use(cookieSession(
+    {
+    name: 'session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys:[config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+    }
+));
+
+app.use(passport.initialize()); //it sets up passport session
+
+app.use(passport.session());
 
 function checkLoggedIn(req, res, next){
     const isLoggedIn = true;
@@ -41,9 +70,9 @@ function checkLoggedIn(req, res, next){
     }
     next();
 }
-app.get('/auth/google', passport.authenticate('google', {
-    scope:['email'],
-}));
+app.get('/auth/google', passport.authenticate('google', 
+    {scope:['email']},
+));
 
 
 
@@ -51,7 +80,7 @@ app.get('/auth/google', passport.authenticate('google', {
 app.get('/auth/google/callback',passport.authenticate('google',{
     failurRedirect: '/failure',
     successRedirect: '/',
-    session: false,
+    session: true,
 }), 
 (req, res) => {
     console.log('Googel called us back!');
